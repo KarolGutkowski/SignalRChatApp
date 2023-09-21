32a50f43c93069edcc1dcaa3dfa29ebb6eabd0f1
Karol Gutkowski <01171298@pw.edu.pl>
2023-09-21 20:11:16 +0200

    enabled cors for local client

--- SignalRChatApp/Program.cs
+++ SignalRChatApp/Program.cs
@@ -1,18 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
 using SignalRChatApp.Hubs;
 using SignalRChatApp.Models;
 using SignalRChatApp.Repositories;
+using Microsoft.AspNetCore.Cors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSignalR();
+builder.Services.AddSignalR((hubOptions) =>
+{
+    hubOptions.EnableDetailedErrors = true;
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IChatRepository, ChatRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowLocalHostConnections",
+        builder=>
+        {
+            builder.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+        });
+});
+
+
 var app = builder.Build();
+app.UseCors("AllowLocalHostConnections");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -22,19 +41,22 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.MapDefaultControllerRoute();
 app.MapHub<ChatHub>("/chathub");
 
-app.MapPost("chathub/{userName}/newmessage", 
-    ([FromRoute] string userName, 
-    [FromBody] MessageDto dto, 
-    IChatRepository auctionRepo) =>
+app.MapPost("chathub/{userName}/newmessage",
+    async ([FromRoute] string userName,
+    [FromBody] MessageDto dto,
+    IChatRepository auctionRepo/*,
+    IHubContext<ChatHub> hubContext*/) =>
 {
     var chatMessage = new ChatMessage(userName, dto.message);
     auctionRepo.addMessage(chatMessage);
+    // await hubContext.Clients.All.SendAsync("ReceiveMessage", chatMessage);
 });
 
 
--- SignalRChatApp/wwwroot/js/site.js
+++ SignalRChatApp/wwwroot/js/site.js
@@ -1,62 +1,60 @@
 ﻿
 const appendNewMessageToChatInterface = (userName, message) => {
-
-    const messageInbox = document.getElementById("msg-page");
-    const receivedMsg = document.createElement("div");
-    receivedMsg.className = "received-msg";
-    receivedMsg.innerHTML = `
-        <div class="received-msg-inbox">
-            <p class="single-msg">${message}</p>
-            <span class="time">18:31 PM | July 24</span>
-        </div>
-    `;
-    messageInbox.appendChild(receivedMsg);
-}
+  const messageInbox = document.getElementById("msg-page");
+  const receivedMsg = document.createElement("div");
+  receivedMsg.className = "received-msg";
+  receivedMsg.innerHTML = `<div class="received-msg-inbox">
+                                <p class="single-msg">${message}</p>
+                                <span class="time">18:31 PM | July 24</span>
+                            </div>`;
+  messageInbox.appendChild(receivedMsg);
+};
 
 const initializeSignalRConnection = (chatRoomName) => {
-    const connection = new signalR.HubConnectionBuilder()
-        .withUrl("/chathub")
-        .build();
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chathub")
+    .build();
 
+  connection.on("ReceiveMessage", ({ userName, message }) => {
+    appendNewMessageToChatInterface(userName, message);
+  });
 
-    connection.on("ReceiveMessage",({ userName, message }) => {
-        appendNewMessageToChatInterface(userName, message);
-    });
+  let timerId;
 
-    let timerId;
+  connection.on("ReceiveSomeoneWriting", () => {
+    const notifyUserWriting = document.getElementById(
+      "another-user-writing-notifiaction"
+    );
+    console.log("someone writing");
+    notifyUserWriting.classList.remove("hidden");
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(() => {
+      notifyUserWriting.classList.add("hidden");
+    }, 1500);
+  });
 
-    connection.on("ReceiveSomeoneWriting", () => {
-        const notifyUserWriting = document.getElementById("another-user-writing-notifiaction");
-        console.log("someone writing");
-        notifyUserWriting.classList.remove('hidden');
-        if (timerId) {
-            clearTimeout(timerId);
-        }
-        timerId = setTimeout(() => {
-            notifyUserWriting.classList.add('hidden');
-        }, 1500);
-    });
+  connection.on("GetAllMessages", async (allMessages) => {
+    for (let msg of allMessages) {
+      await appendNewMessageToChatInterface(msg.userName, msg.message);
+    }
+  });
 
-    connection.on("GetAllMessages", async (allMessages) => {
-        for (let msg of allMessages) {
-            await appendNewMessageToChatInterface(msg.userName, msg.message);
-        }
-    });
-
-    connection.start().catch(err => console.error(err.toSring()));
-    return connection;
-}
+  connection.start().catch((err) => console.error(err.toSring()));
+  return connection;
+};
 
 const connection = initializeSignalRConnection();
 
 const send = () => {
-    var userName = "placeholder";
-    var messageInput = document.getElementById("message-input");
-    var message = messageInput.value;
-    messageInput.value = "";
+  var userName = "placeholder";
+  var messageInput = document.getElementById("message-input");
+  var message = messageInput.value;
+  messageInput.value = "";
 
-    const messagePage = document.getElementById("msg-page");
-    var messageMarkup = `<div class="outgoing-chats">
+  const messagePage = document.getElementById("msg-page");
+  var messageMarkup = `<div class="outgoing-chats">
                                 <div class="outgoing-msg">
                                   <div class="outgoing-chats-msg">
                                     <p>
@@ -65,33 +63,33 @@ const send = () => {
                                     <span class="time">18:34 PM | July 24</span>
                                   </div>
                                 </div>
-                            </div>`
-    messagePage.innerHTML += messageMarkup;
+                            </div>`;
+  messagePage.innerHTML += messageMarkup;
 
-    scrollToBottom(messagePage);
+  scrollToBottom(messagePage);
 
-    console.log(userName + " " + message);
+  console.log(userName + " " + message);
 
-    fetch(`/chathub/${userName}/newmessage`, {
-        method: "POST",
-        body: JSON.stringify({ message }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  fetch(`/chathub/${userName}/newmessage`, {
+    method: "POST",
+    body: JSON.stringify({ message }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    connection.invoke("NotifyNewMessage", {
-        username: userName,
-        message: message
-    });
-}
+  connection.invoke("NotifyNewMessage", {
+    username: userName,
+    message: message,
+  });
+};
 
 const notifyUserWritingMessage = () => {
-    console.log("user writing message");
+  console.log("user writing message");
 
-    connection.invoke("NotifyThatImWriting");
-}
+  connection.invoke("NotifyThatImWriting");
+};
 
 function scrollToBottom(element) {
-    element.scrollTop = element.scrollHeight;
+  element.scrollTop = element.scrollHeight;
 }
